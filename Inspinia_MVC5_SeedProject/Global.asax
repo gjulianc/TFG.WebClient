6bb2167a0e5be1c8f192753bb8dfe5a5d4a9a20e
Gabriel <gjulian_es@yahoo.es>
2016-05-13 19:35:28 +0200

    Agregar archivos de proyecto.

---
+++ Inspinia_MVC5_SeedProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TFG.WebClient.MvcApplication" Language="C#" %>
---
+++ Inspinia_MVC5_SeedProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TFG.WebClient.Startup))]
+namespace TFG.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
